55f5f0306255196b5f6efc114b54036c8c295a0a
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-11-19 14:12:41 +0530

    login and signup designing done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
 const http = require('http')
+const ejs = require("ejs")
 const server=http.createServer(app)
+app.set("view engine","ejs")
+app.use(express.static('public'))
+app.use(express.json());
 
 
 app.get("/",(req,res)=>
@@ -10,6 +14,15 @@ app.get("/",(req,res)=>
 })
 
 
+app.get("/login",(req,res)=>
+{
+  res.render("login")
+})
+
+app.get("/signup",(req,res)=>
+{
+    res.render("signup")
+})
 server.listen(1000)
 
 
