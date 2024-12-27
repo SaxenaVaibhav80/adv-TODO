a98967cd1419770fbc13b8a30fb5f40f8cb676aa
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-12-27 21:49:52 +0530

    added tasks api

---
+++ tasks/task.js
@@ -0,0 +1,12 @@
+const tasks = [
+    "Exercise for 30-40 minutes daily to stay healthy and keep yourself fit.",
+    "Maintain your study or work space to ensure a clean environment for better focus..",
+    "Eat fruits that will reduce vitamin deficiencies from your body and keep you healthy..",
+    "Donate food or clothes to collect blessings and positively influence others.",
+    "Work on skills in your field (whether tech or non-tech) to improve professionally.",
+    "Learn at least one new thing related to your field in depth to enhance your skills.",
+    "Engage in group discussions or english speaking to improve your spoken skills.",
+    "Write articles or research papers to expand your knowledge and expertise.",
+    "Solve a puzzle today to sharp your mind that will enhance your problem solving skills."
+  ];
+  module.exports = tasks;
