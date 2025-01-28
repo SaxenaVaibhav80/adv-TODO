28d653f0a14f7cf8888e0be993b5c74a79ce3bd7
Vanshika2208 <116169555+Vanshika2208@users.noreply.github.com>
2025-01-28 21:59:56 +0530

    basic history

--- public/js/history.js
+++ public/js/history.js
@@ -1,1 +1,28 @@
 alert("hii")
+function themestate() {
+    fetch("/themeState", {
+        method: "POST"
+    }).then(res => res.text())
+    .then((modes) => {
+        const body = document.body;
+        const lightOuter = document.getElementById("lightouter");
+        const light = document.getElementById("light");
+        const lmode = document.getElementById("lmode");
+
+        if (modes === "Dark mode") {
+            body.classList.add("dark-mode");
+
+            lightOuter.classList.remove("shift-left");
+            lightOuter.classList.add("shift-right");
+            light.setAttribute("src", "/img/dark-mode.png");
+            lmode.innerHTML = "Dark mode";
+        } else {
+            body.classList.remove("dark-mode");
+
+            lightOuter.classList.remove("shift-right");
+            lightOuter.classList.add("shift-left");
+            light.setAttribute("src", "/img/lightmode.png");
+            lmode.innerHTML = "Light mode";
+        }
+    }); 
+}
